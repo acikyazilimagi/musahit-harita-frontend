1c6b2203f5780b56841b8378b49aaef0919e91b9
usirin <umutsirin1@gmail.com>
2023-05-22 19:47:33 -0700

    feat: use new data model to implement dropdown functionality

--- src/features/location-categories/voting-locations/useVotingLocationsData.ts
+++ src/features/location-categories/voting-locations/useVotingLocationsData.ts
@@ -2,16 +2,19 @@ import { useEffect } from "react";
 import { useMap } from "react-leaflet";
 import { useVotingLocations } from "./useVotingLocations";
 
+const ZOOM_LEVEL_CITY = 9;
+const ZOOM_LEVEL_DISTRICT = 11;
+const ZOOM_LEVEL_NEIGHBORHOOD = 13;
+
 export const useVotingLocationsData = () => {
-  const { selectedDistrict, selectedCity } = useVotingLocations();
+  const { selectedNeighborhood, selectedDistrict, selectedCity } =
+    useVotingLocations();
   const map = useMap();
 
   // city is changed: zoom into city
   useEffect(() => {
     if (!selectedCity) return;
-    if (!("latitude" in selectedCity) || !("longitude" in selectedCity)) return;
-
-    map.setView([selectedCity.latitude, selectedCity.longitude], 9, {
+    map.setView([selectedCity.lat, selectedCity.lng], ZOOM_LEVEL_CITY, {
       animate: true,
     });
   }, [map, selectedCity]);
@@ -19,11 +22,21 @@ export const useVotingLocationsData = () => {
   // district is changed: zoom into district
   useEffect(() => {
     if (!selectedDistrict) return;
-    if (!("latitude" in selectedDistrict) || !("longitude" in selectedDistrict))
-      return;
 
-    map.setView([selectedDistrict.latitude, selectedDistrict.longitude], 11, {
-      animate: true,
-    });
+    map.setView(
+      [selectedDistrict.lat, selectedDistrict.lng],
+      ZOOM_LEVEL_DISTRICT,
+      { animate: true }
+    );
   }, [map, selectedDistrict]);
+
+  // neighborhood is changed: zoom into neighborhood
+  useEffect(() => {
+    if (!selectedNeighborhood) return;
+    map.setView(
+      [selectedNeighborhood.lat, selectedNeighborhood.lng],
+      ZOOM_LEVEL_NEIGHBORHOOD,
+      { animate: true }
+    );
+  }, [map, selectedNeighborhood]);
 };
