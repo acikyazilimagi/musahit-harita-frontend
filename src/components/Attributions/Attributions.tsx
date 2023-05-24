c8e0043a4a1cc94878555cf465ec35a966d057ef
usirin <umutsirin1@gmail.com>
2023-05-24 16:53:49 -0700

    feat: remove data sources info

--- src/components/Attributions/Attributions.tsx
+++ src/components/Attributions/Attributions.tsx
@@ -1,11 +1,7 @@
 import { useTranslation } from "next-i18next";
 import styles from "./Attributions.module.css";
 
-interface AttributionComponentProps {
-  onClick?: () => void;
-}
-
-export function AttributionComponent(props: AttributionComponentProps) {
+export function AttributionComponent() {
   const { t } = useTranslation("common");
   return (
     <div className={styles.attribution}>
@@ -14,9 +10,6 @@ export function AttributionComponent(props: AttributionComponentProps) {
       </a>
       <a href="./gizlilik.pdf" target="_blank">
         {t("footer.politic.privacy")}
-      </a>
-      <a href="#" onClick={props.onClick}>
-        {t("footer.politic.data")}
       </a>
       <a href="https://maps.google.com/">Google Maps</a>
       <a href="https://leafletjs.com/">Leaflet</a>
