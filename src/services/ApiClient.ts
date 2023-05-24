49954edbca132317383eadd8f8fb574134dc1cee
usirin <umutsirin1@gmail.com>
2023-05-23 17:04:55 -0700

    feat: add fetchFeeds method to ApiClient

--- src/services/ApiClient.ts
+++ src/services/ApiClient.ts
@@ -1,11 +1,44 @@
-type ApiClientProps = {
-  url: string;
+import { API_URL } from "@/env";
+
+const dataFetcher = async <T>(url: string | URL) => {
+  const response = await fetch(url);
+  return (await response.json()) as T;
 };
+
+interface ApiResponseFeeds {
+  count: number;
+  results: ApiResponseIntensity[];
+}
+
+interface ApiResponseIntensity {
+  neighborhood_id: number;
+  volunteer_data: number;
+}
+
+interface IntensityData {
+  neighborhoodID: number;
+  intensity: number;
+}
+
+const transformResponse = (response: ApiResponseIntensity): IntensityData => ({
+  neighborhoodID: response.neighborhood_id,
+  intensity: response.volunteer_data,
+});
+
+interface ApiClientProps {
+  url?: string;
+}
 
 export class ApiClient {
   url: string;
 
   constructor(props: ApiClientProps) {
-    this.url = props.url;
+    this.url = props.url ?? API_URL;
+  }
+
+  async fetchFeeds() {
+    const url = new URL(this.url + "/feeds/mock");
+    const { results } = await dataFetcher<ApiResponseFeeds>(url);
+    return results.map(transformResponse);
   }
 }
