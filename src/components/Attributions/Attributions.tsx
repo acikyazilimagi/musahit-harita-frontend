da8135ccc108884b267d7e515bc5080bc1ad2edb
andyanday33 <andyanday33@gmail.com>
2023-05-26 00:38:03 +0200

    chore: update cookie document, remove privacy document and related links

--- src/components/Attributions/Attributions.tsx
+++ src/components/Attributions/Attributions.tsx
@@ -8,9 +8,6 @@ export function AttributionComponent() {
       <a href="./cerez.pdf" target="_blank">
         {t("footer.politic.cookie")}
       </a>
-      <a href="./gizlilik.pdf" target="_blank">
-        {t("footer.politic.privacy")}
-      </a>
       <a href="https://maps.google.com/">Google Maps</a>
       <a href="https://leafletjs.com/">Leaflet</a>
     </div>
