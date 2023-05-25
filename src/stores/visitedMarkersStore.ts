a07a92d12ca69cb331ca9a7d0afd6d2594d488ad
Deniz Gurkaynak <dgurkaynak@gmail.com>
2023-05-25 23:28:41 +0300

    feat: persist only `mapGeographyStore` in the URL

--- src/stores/visitedMarkersStore.ts
+++ src/stores/visitedMarkersStore.ts
@@ -1,5 +1,4 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
 
 interface MapState {
   visitedMarkers: Record<number, number>;
@@ -7,20 +6,13 @@ interface MapState {
   isVisited: (_id: number) => boolean;
 }
 
-export const useVisitedMarkersStore = create<MapState>()(
-  persist(
-    (set, get) => ({
-      visitedMarkers: {},
-      setVisited: (id: number) => {
-        const visitedMarkers = get().visitedMarkers;
-        set({ visitedMarkers: { ...visitedMarkers, [id]: id } });
-      },
-      isVisited: (id: number) => {
-        return !!get().visitedMarkers[id];
-      },
-    }),
-    {
-      name: "visitedMarkers",
-    }
-  )
-);
+export const useVisitedMarkersStore = create<MapState>()((set, get) => ({
+  visitedMarkers: {},
+  setVisited: (id: number) => {
+    const visitedMarkers = get().visitedMarkers;
+    set({ visitedMarkers: { ...visitedMarkers, [id]: id } });
+  },
+  isVisited: (id: number) => {
+    return !!get().visitedMarkers[id];
+  },
+}));
