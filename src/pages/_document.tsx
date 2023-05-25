9e301b93e98e5f0ea1ee20eeec65e7b674942bbe
usirin <umutsirin1@gmail.com>
2023-05-25 14:57:52 -0700

    feat: update favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="shortcut icon" href="logo.svg" />
+          <link rel="shortcut icon" href="icon.png" />
           <meta name="emotion-insertion-point" content="" />
           {(this.props as any).emotionStyleTags}
         </Head>
