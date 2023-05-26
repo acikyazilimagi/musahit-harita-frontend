ad53542f0cde43f2bf2a9c435360a59e29b59662
puskuruk <tamayeseruysal@gmail.com>
2023-05-26 19:38:27 +0200

    feat: Change marker color according to intensity

--- src/components/Map/Cluster/GenericClusterGroup.tsx
+++ src/components/Map/Cluster/GenericClusterGroup.tsx
@@ -15,15 +15,50 @@ const fetchIcon = (count: number) => {
   });
 };
 
-const markerBlueIcon = L.Icon.Default.extend({
-  options: {},
-});
-
-const markerGrayIcon = L.Icon.Default.extend({
-  options: {
-    className: styles.marker_icon__visited,
-  },
-});
+function getSVGMarker({ color }: { color: string } = { color: "#FF6E6E" }) {
+  // icon URL: https://www.svgrepo.com/svg/302636/map-marker
+  return L.divIcon({
+    html: `
+      <svg
+      viewBox="-4 0 36 36"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g
+        stroke="none"
+        stroke-width="1"
+        fill="none"
+        fill-rule="evenodd"
+      >
+        <g id="Vivid-Icons" transform="translate(-125.000000, -643.000000)">
+          <g id="Icons" transform="translate(37.000000, 169.000000)">
+            <g id="map-marker" transform="translate(78.000000, 468.000000)">
+              <g transform="translate(10.000000, 6.000000)">
+                <path
+                  d="M14,0 C21.732,0 28,5.641 28,12.6 C28,23.963 14,36 14,36 C14,36 0,24.064 0,12.6 C0,5.641 6.268,0 14,0 Z"
+                  id="Shape"
+                  fill=${color}
+                ></path>
+                <circle
+                  id="Oval"
+                  fill="#fff"
+                  fill-rule="nonzero"
+                  cx="14"
+                  cy="14"
+                  r="7"
+                ></circle>
+              </g>
+            </g>
+          </g>
+        </g>
+      </g>
+    </svg>
+  `,
+    className: "svg-marker-icon",
+    iconSize: [28, 28],
+    iconAnchor: [14, 14],
+  });
+}
 
 type Props = {
   data: ChannelData[];
@@ -94,17 +129,10 @@ export const GenericClusterGroup = ({ data, onMarkerClick }: Props) => {
             position={[latitude, longitude]}
             icon={
               cluster.properties.icon
-                ? L.icon({
-                    iconUrl: "/" + cluster.properties.icon,
-                    iconSize: [28, 28],
-                    iconAnchor: [14, 14],
-                    className: isVisited(cluster.item.reference)
-                      ? styles.marker_icon__visited
-                      : "",
-                  })
+                ? getSVGMarker()
                 : isVisited(cluster.item.reference)
-                ? new markerGrayIcon()
-                : new markerBlueIcon()
+                ? getSVGMarker({ color: "#353535" })
+                : getSVGMarker({ color: "#193866" })
             }
             eventHandlers={{
               click: (e) => {
