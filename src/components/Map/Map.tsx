4f4ab2cd2b42e41e7d2a855f277557232613b879
eraygundogmus <eraygundogmus@teknasyon.com>
2023-05-27 19:23:16 +0300

    feat: add header

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,21 +1,149 @@
 import { MapOptions } from "leaflet";
 import dynamic from "next/dynamic";
 import { ReactNode } from "react";
+import { HelpOutline } from "@mui/icons-material";
+import InfoOutlinedIcon from "@mui/icons-material/InfoOutlined";
+import { useAboutView } from "@/components/AboutUs/AboutUsOverlay";
+import { useHelpView } from "@/components/UserGuide/UserGuide";
+import { IconButton, Theme, SxProps } from "@mui/material";
+import Image from "next/image";
 
 const DynamicMap = dynamic(() => import("./Dynamic"), {
   ssr: false,
 });
-
+interface IStyles {
+  [key: string]: SxProps<Theme>;
+}
 interface MapProps extends MapOptions {
   children: ReactNode;
   // eslint-disable-next-line no-unused-vars
   whenReady: (map: any) => void;
 }
+const styles: IStyles = {
+  button: (theme: Theme) => ({
+    backgroundColor: theme.palette.common.white,
+    color: `${theme.palette.grey[600]} !important`,
+    borderRadius: "8px !important",
+    fontSize: {
+      xs: "0.8rem",
+    },
+    display: "flex",
+    alignItems: "center",
+    gap: {
+      xs: "2px",
+      sm: "4px",
+      md: "8px",
+    },
+    padding: {
+      xs: "0",
+      sm: "4px 8px",
+    },
+    svg: {
+      fontSize: {
+        xs: "1rem",
+        sm: "1.4rem",
+      },
+    },
+    "&:hover": {
+      backgroundColor: theme.palette.common.white,
+    },
+  }),
+};
 
 export const Map = (props: MapProps) => {
+  const helpView = useHelpView();
+  const aboutView = useAboutView();
   return (
-    <div style={{ width: "100vw", height: "100dvh" }}>
-      <DynamicMap {...props} />
-    </div>
+    <>
+      <header
+        style={{
+          backgroundColor: "#fff",
+          borderBottom: "1px solid #EAECF0",
+          boxShadow: "0 4px 30px rgba(0, 0, 0, 0.1)",
+          position: "fixed",
+          top: "0px",
+          height: "60px",
+          padding: "0px 16px",
+          width: "100%",
+          zIndex: 1030,
+          display: "flex",
+          justifyContent: "space-between",
+        }}
+      >
+        <div>
+          <Image
+            src="/logo.svg"
+            alt="logo"
+            loading="eager"
+            width={100}
+            height={60}
+            style={{
+              transform: "scale(3)",
+            }}
+          />
+        </div>
+
+        <ul
+          style={{
+            display: "flex",
+            listStyle: "none",
+            gap: "12px",
+            margin: "0px",
+            padding: "0px",
+          }}
+        >
+          <li
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              paddingBottom: "14px",
+            }}
+          >
+            <IconButton
+              sx={styles.button}
+              color="inherit"
+              onClick={() => {
+                helpView.toggle(!helpView.isOpen);
+                aboutView.toggle(false);
+              }}
+            >
+              User Guide
+              <HelpOutline />
+            </IconButton>
+          </li>
+          <li
+            style={{
+              display: "flex",
+              alignItems: "flex-end",
+              paddingBottom: "14px",
+            }}
+          >
+            <IconButton
+              sx={styles.button}
+              color="inherit"
+              onClick={() => {
+                aboutView.toggle(!aboutView.isOpen);
+                helpView.toggle(false);
+              }}
+            >
+              About Us
+              <InfoOutlinedIcon />
+            </IconButton>
+          </li>
+        </ul>
+      </header>
+      <div
+        style={{
+          width: "100vw",
+          height: "100% - 60px",
+          top: "60px",
+          bottom: "0",
+          position: "absolute",
+          zIndex: 1000,
+        }}
+      >
+        <DynamicMap {...props} />
+      </div>
+    </>
   );
 };
