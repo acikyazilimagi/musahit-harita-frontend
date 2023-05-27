57e2601dbbb325ffeedf9ce1673277b854c65f42
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-05-27 19:00:16 +0300

    feat(#136): update path for mock feeds

--- src/features/api-client/ApiClient.ts
+++ src/features/api-client/ApiClient.ts
@@ -64,13 +64,13 @@ export class ApiClient {
   }
 
   async fetchFeeds() {
-    const url = new URL(this.url + "/feeds");
+    const url = new URL(this.url + "/mock/feeds");
     const data = await dataFetcher<ApiResponseFeeds>(url);
     return data ? data.results.map(transformResponse) : null;
   }
 
   async fetchDetail(id: string) {
-    const url = new URL(this.url + "/feed/" + id);
+    const url = new URL(this.url + "/mock/feed/" + id);
     const data = await dataFetcher<ApiResponseFeed>(url);
     return data ? transformDetailResponse(data) : null;
   }
